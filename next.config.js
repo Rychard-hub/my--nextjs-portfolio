58824783de19b47eec1ee8839973367e7cb61cff
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2022-11-04 20:12:59 +0200

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  distDir: 'build',
+  distDir: '.next',
 }
 
 module.exports = nextConfig
